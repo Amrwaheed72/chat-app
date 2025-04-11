df0a4f775e6f0446fcec362ff74489bb51101221
Amr Waheed <amrmohamed877@gmail.com>
2025-04-11 20:42:14 +0200

    finishing setting up the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
-import { BrowserRouter } from 'react-router-dom';
-
+import { Route, Routes } from 'react-router-dom';
+import './app.css';
+import Navbar from './ui/Navbar';
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import SettingsPage from './pages/SettingsPage';
+import ProfilePage from './pages/ProfilePage';
 function App() {
     const queryClient = new QueryClient({
         defaultOptions: {
@@ -13,28 +19,37 @@ function App() {
     });
     return (
         <QueryClientProvider client={queryClient}>
-            <BrowserRouter></BrowserRouter>
-            <ReactQueryDevtools initialIsOpen={false} />
-            <Toaster
-                position="top-center"
-                gutter={12}
-                containerStyle={{ margin: '8px' }}
-                toastOptions={{
-                    success: {
-                        duration: 3000,
-                    },
-                    error: {
-                        duration: 5000,
-                    },
-                    style: {
-                        fontSize: '16px',
-                        maxWidth: '500px',
-                        padding: '16px 24px',
-                        backgroundColor: '#9db787 ',
-                        color: 'black',
-                    },
-                }}
-            />
+            <div>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/signup" element={<SignUpPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/settings" element={<SettingsPage />} />
+                    <Route path="/profile" element={<ProfilePage />} />
+                </Routes>
+                <ReactQueryDevtools initialIsOpen={false} />
+                <Toaster
+                    position="top-center"
+                    gutter={12}
+                    containerStyle={{ margin: '8px' }}
+                    toastOptions={{
+                        success: {
+                            duration: 3000,
+                        },
+                        error: {
+                            duration: 5000,
+                        },
+                        style: {
+                            fontSize: '16px',
+                            maxWidth: '500px',
+                            padding: '16px 24px',
+                            backgroundColor: '#9db787 ',
+                            color: 'black',
+                        },
+                    }}
+                />
+            </div>
         </QueryClientProvider>
     );
 }
